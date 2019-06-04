8deeddcd223930e0c06a5958d7f7ebd1f4256776
Andrii <gaarasandy@gmail.com>
2019-06-04 20:33:41 +0300

    styled code

--- main.js
+++ main.js
@@ -6,34 +6,35 @@ document.addEventListener('DOMContentLoaded', function () {
       .then(response => response.json());
   }
   Promise.all(
-    [getUrl('https://jsonplaceholder.typicode.com/todos'),
-      getUrl('https://jsonplaceholder.typicode.com/users')]
-   ).then(data => todoItems(data));
+    [
+      getUrl('https://jsonplaceholder.typicode.com/todos'),
+      getUrl('https://jsonplaceholder.typicode.com/users')
+    ]
+  ).then(data => todoItems(data));
 
 
-   const todoItems = (data) => {
+  const todoItems = (data) => {
     console.log(data);
     const toDoList = data[0];
     const userNames = data[1];
     const container = document.querySelector('.container');
     const titleName = ['TODO', 'User name', 'Status'];
-    
 
-   return container.innerHTML = `
+
+    return container.innerHTML = `
       <table class="table">
         <thead>
           <tr>${titleName.map(item => `<td>${item}</td>`).join('')}</tr>
         </thead>
         <tbody>
-          ${toDoList.map(item =>`<tr>
-                                    <td>${item.title}</td>
-                                    <td><a href=${userNames[item.userId - 1].email}>${userNames[item.userId - 1].name}</a></td>
-                                    <td>${item.completed}</td>
-                                </tr>`).join('')}
+          ${toDoList.map(item =>`
+          <tr>
+            <td>${item.title}</td>
+            <td><a href=${userNames[item.userId - 1].email}>${userNames[item.userId - 1].name}</a></td>
+            <td>${item.completed}</td>
+          </tr>`).join('')}
         </tbody>
       </table>
     `
   }
-  
 })
-

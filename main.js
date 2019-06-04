872d0e3b505eedc9af649064017e0af8a799ebe8
Andrii <gaarasandy@gmail.com>
2019-06-04 19:58:59 +0300

    solution

--- main.js
+++ main.js
@@ -1,43 +1,39 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function () {
-  const table = document.querySelector('.table');
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos');
-  xhr.responseType = 'json';
-  xhr.send();
-  xhr.addEventListener('load', function () {
-    let todoColumn = xhr.response;
-    todoColumn.forEach(item => {
-      const tr = document.createElement('tr');
-      const td = document.createElement('td');
-      tr.setAttribute('class', `id-${item.userId}`);
-      td.textContent = item.title;
-      tr.append(td);
-      table.append(tr);
-    })
-    const request = new XMLHttpRequest();
-    request.open('GET', 'https://jsonplaceholder.typicode.com/users');
-    request.responseType = 'json';
-    request.send();
-    request.addEventListener('load', function () {
-      let namesColumn = request.response;
-      namesColumn.forEach(item => {
-        const index = item.id;
-        let rows = document.querySelectorAll(`.id-${index}`);
-        rows.forEach(el => {
-          if(el.getAttribute("class") === `id-${item.id}`) {
-            const td = document.createElement('td');
-            td.innerHTML = `<a href="${item.email}">${item.name}</a>`;
-            el.append(td);
-          }
-        })
-      
-      })
-
-      console.log(namesColumn);
-    })
-  });
+  function getUrl(url) {
+    return fetch(url)
+      .then(response => response.json());
+  }
+  Promise.all(
+    [getUrl('https://jsonplaceholder.typicode.com/todos'),
+      getUrl('https://jsonplaceholder.typicode.com/users')]
+   ).then(data => todoItems(data));
 
 
-});
+   const todoItems = (data) => {
+    console.log(data);
+    const toDoList = data[0];
+    const userNames = data[1];
+    const container = document.querySelector('.container');
+    const titleName = ['TODO', 'User name', 'Status'];
+    
+
+   return container.innerHTML = `
+      <table class="table">
+        <thead>
+          <tr>${titleName.map(item => `<td>${item}</td>`).join('')}</tr>
+        </thead>
+        <tbody>
+          ${toDoList.map(item =>`<tr>
+                                    <td>${item.title}</td>
+                                    <td><a href=${userNames[item.userId - 1].email}>${userNames[item.userId - 1].name}</a></td>
+                                    <td>${item.completed}</td>
+                                </tr>`).join('')}
+        </tbody>
+      </table>
+    `
+  }
+  
+})
+

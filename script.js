641c7082758b2f93a544598a9f7af1d34af2d07f
Alexandr <alexandrmail32@gmail.com>
2019-06-06 15:07:17 +0300

    Refactoring

--- script.js
+++ script.js
@@ -1,37 +1,33 @@
 const requests = ['todos', 'users'];
-const data = [];
+const data = {};
 
   requests.forEach(function(item){
     const request = new XMLHttpRequest();
-    request.open('GET', `https://jsonplaceholder.typicode.com/${item}`, false);
+    request.open('GET', `https://jsonplaceholder.typicode.com/${item}`);
     request.addEventListener('load', function() {
-    data.push(JSON.parse(this.response));
+    data[item] = JSON.parse(this.response);
+      if (data.todos && data.users) {
+        renderTable(data.todos, data.users);
+      }
     });
     request.send();
   })
-  const [todos, users] = data;
- 
-  renderTable(todos, users);
 
  function renderTable(todos, users) {
   let table = document.querySelector('#table');
-  let userName;
-  let userEmail;
+  
   table.insertAdjacentHTML('beforeend', `
     <thead>
       <th>Title</th><th>Name</th><th>Status</th>
     </thead>
     <tbody></tbody`);
+    
   for (let item of todos) {
-    let isCompleted;
-    item.completed ? isCompleted = 'completed' : isCompleted = 'not completed'
-    for (let user of users) {
-      if(item.userId === user.id) {
-        userName = user.name;
-        userEmail = user.email;
-        break;
-      }
-    }
+    const isCompleted = item.completed ? 'completed' : 'not completed'
+    const user = users.filter(user => user.id === item.userId);
+    const userName = user[0].name;
+    const userEmail = user[0].email;
+
     table.tBodies[0].insertAdjacentHTML('beforeend', `
     <tr>
       <td>${item.title}</td><td><a href="mailto:${userEmail}">${userName}</a></td><td>${isCompleted}</td>

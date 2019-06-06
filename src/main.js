ed538f062b574f72e817fde850c27c1782f946fd
Ekaterina Kurochka <ekaterina1.kur@gmail.com>
2019-06-06 17:18:09 +0300

    With fetch

--- src/main.js
+++ src/main.js
@@ -1,29 +1,37 @@
 const table = document.querySelector('.todos');
 
-function CreateTodosList(url) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', url, false);
-    xhr.send();
-    const parsedData = JSON.parse(xhr.responseText);
-
-    return parsedData;
+function loadApi(url) {
+  return fetch(url)
+    .then(responce => responce.json())
+    .then(data => data)
 }
-
-const items = CreateTodosList('https://jsonplaceholder.typicode.com/todos');
-const users = CreateTodosList('https://jsonplaceholder.typicode.com/users');
+  
+function loadItems() {
+  Promise.all([
+    loadApi('https://jsonplaceholder.typicode.com/todos'),
+    loadApi('https://jsonplaceholder.typicode.com/users'),
+  ])
+  .then(([items, users]) => {
+    return render(items, users);
+  })
+}
+  
+loadItems();
 
 function createTemplate(item, user) {
-    return `
-        <tr>
-            <td>${item.title}</td>
-            <td><a href="mailto:${user.email}">${user.name}</a></td>
-            <td>${item.completed}</td>
-        </tr>
-    `;
+  return `
+    <tr>
+      <td>${item.title}</td>
+      <td><a href="mailto:${user.email}">${user.name}</a></td>
+      <td>${item.completed}</td>
+    </tr>
+  `;
 }
 
-items.forEach(item => {
+function render(items, users) {
+  items.forEach(item => {
     const owner = users.find((user) => user.id === item.userId);
     const tr = createTemplate(item, owner);
     table.insertAdjacentHTML('beforeend', tr);
-});
+  });
+}

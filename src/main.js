52634ba1ed96b8329f2c1e6e7908838033c9e195
Ivan Deiko <ivandeiko1997@gmail.com>
2019-06-14 10:35:43 +0300

    'fix'

--- src/main.js
+++ src/main.js
@@ -10,9 +10,9 @@ function getDate(url) {
 Promise.all(
   [getDate('https://jsonplaceholder.typicode.com/todos'), 
    getDate('https://jsonplaceholder.typicode.com/users')],
-).then(data => {
-    data[0].forEach(todo => {
-     const row = createRows(todo, data[1].find(user => user.id === todo.userId));
+).then(([todos, users]) => {
+    todos.forEach(todo => {
+     const row = createRows(todo, users.find(user => user.id === todo.userId));
      table.insertAdjacentHTML('beforeend', row);
     })
 });

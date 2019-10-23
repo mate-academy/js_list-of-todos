1eb4a6bcbe68e2ce824b1431e15af7e094ac3d45
Viktor Lukovets <nuerio63@gmail.com>
2019-10-23 15:47:08 +0300

    testing spaces

--- scripts/main.js
+++ scripts/main.js
@@ -9,18 +9,18 @@ async function loadData(url) {
 }
 
 async function completeTable() {
-	const todoList = await loadData('https://jsonplaceholder.typicode.com/todos');
-	const users = await loadData('https://jsonplaceholder.typicode.com/users');
-	for (let i = 0; i < todoList.length; i++) {
-		const tr = document.createElement('TR');
-		const currentUser = users.find(user => user.id == todoList[i].userId);
-		tr.innerHTML = `
-			<td>${todoList[i].title}</td>
-			<td><a href="mailto:${currentUser.email}">${currentUser.name}<a></td>
-			<td>${todoList[i].completed}</td>
-		`;
-		tbody.append(tr);
-	}
+  const todoList = await loadData('https://jsonplaceholder.typicode.com/todos');
+  const users = await loadData('https://jsonplaceholder.typicode.com/users');
+  for (let i = 0; i < todoList.length; i++) {
+    const tr = document.createElement('TR');
+    const currentUser = users.find(user => user.id == todoList[i].userId);
+    tr.innerHTML = `
+      <td>${todoList[i].title}</td>
+      <td><a href="mailto:${currentUser.email}">${currentUser.name}<a></td>
+      <td>${todoList[i].completed}</td>
+    `;
+    tbody.append(tr);
+  }
 }
 
 completeTable();

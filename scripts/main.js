3f92937f8b942c5ca676e4d5144ca28a9e17f894
Viktor Lukovets <nuerio63@gmail.com>
2019-10-24 11:35:49 +0300

    Solution;

--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,7 @@ const tbody = document.querySelector('#tbody');
 
 async function loadData(url) {
   const response = await fetch(url);
-  const json = await response.json();
-  return json;
+  return response.json();
 }
 
 async function completeTable() {
@@ -13,11 +12,11 @@ async function completeTable() {
   const users = await loadData('https://jsonplaceholder.typicode.com/users');
   for (let i = 0; i < todoList.length; i++) {
     const tr = document.createElement('TR');
-    const currentUser = users.find(user => user.id == todoList[i].userId);
+    const currentUser = users.find(user => user.id === todoList[i].userId);
     tr.innerHTML = `
-      <td>${todoList[i].title}</td>
-      <td><a href="mailto:${currentUser.email}">${currentUser.name}<a></td>
-      <td>${todoList[i].completed}</td>
+      <td class="cell">${todoList[i].title}</td>
+      <td class="cell"><a href="mailto:${currentUser.email}">${currentUser.name}<a></td>
+      <td class="cell">${todoList[i].completed}</td>
     `;
     tbody.append(tr);
   }

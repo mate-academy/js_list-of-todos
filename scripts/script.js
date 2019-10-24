98ec722a497c2bff5a2e59c1a12a7250c02ac8cc
Serhii <gespenst.serge@yandex.ru>
2019-10-24 12:56:08 +0300

    sol

--- scripts/script.js
+++ scripts/script.js
@@ -8,13 +8,14 @@ async function gettingInfo(url)  {
 async function creatingTable() {
   const toDoList = await gettingInfo('https://jsonplaceholder.typicode.com/todos');
   const usersList = await gettingInfo('https://jsonplaceholder.typicode.com/users');
-  
+  console.log(toDoList);
   for (const key of toDoList) {
     const tableRow = document.createElement('tr');
     const userEmail = usersList.find(user => user.id === key.userId).email;
+    const taskProgress = key.completed ? 'green checkmark icon' : 'window close icon red';
     tableRow.innerHTML = `<td>${key.title}</td>
                     <td><a href = "mailto:${userEmail}">${userEmail}</a></td>
-                    <td>${key.completed}</td>`;
+                    <td><i class="${taskProgress}"></i>${key.completed}</td>`;
     tableBody.append(tableRow);
   }
 }

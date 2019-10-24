b53718b070aa54aa2c979222e5ed69c3aa105033
dsrtF0x-git <54433971+dsrtF0x-git@users.noreply.github.com>
2019-10-24 15:14:14 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 'use strict';
-const tableBody = document.getElementsByTagName('tbody')[0];
+const tableBody = document.getElementById('tableBody');
 
 function gettingInfo(url)  {
   return fetch(url).then(response => response.json());
@@ -12,11 +12,12 @@ async function creatingTable() {
   for (const key of toDoList) {
     const tableRow = document.createElement('tr');
     const userEmail = usersList.find(user => user.id === key.userId).email;
+    const userName = usersList.find(user => user.id === key.userId).name;
     const taskProgress = key.completed ? 'green checkmark icon' : 'window close icon red';
     
     tableRow.innerHTML = `<td>${key.title}</td>
-                          <td><a href = "mailto:${userEmail}">${userEmail}</a></td>
-                          <td><i class="${taskProgress}"></i>${key.completed}</td>`;
+                    <td><a href = "mailto:${userEmail}">${userName}</a></td>
+                    <td><i class="${taskProgress}"></i>${key.completed}</td>`;
     
     tableBody.append(tableRow);
   }

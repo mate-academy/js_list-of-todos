14d2c83d7d82a0d69bacc6523a8f315675dc260a
Chepkyi Mykola <chepkiyn@gmail.com>
2019-10-24 17:17:13 +0300

    Solution

--- main.js
+++ main.js
@@ -6,30 +6,32 @@ table.innerHTML = `
       <th>Completed</th>
     </thead>
   `
-  const tableBody = document.createElement('tbody');
-  tableBody.classList.add('tableBody');
-  const list = async function () {
-    const askForTodo = await fetch(`https://jsonplaceholder.typicode.com/todos`);
-    const toDoList = await askForTodo.json();
-    const askForUsers = await fetch(`https://jsonplaceholder.typicode.com/users`)
-    const usersList = await askForUsers.json();
-    const userEmailMap = new Map();
-    const userNameMap = new Map();
-    for (let user of usersList) {
-      userEmailMap.set(user.id, user.email);
-      userNameMap.set(user.id, user.name);
-    }
-    for (let toDo of toDoList) {
-      const row = document.createElement('tr');    
-      const columnToDo = document.createElement('td');    
-      const columnComplete = document.createElement('td');
-      const columnName = document.createElement('td');  
-      columnToDo.textContent = toDo.title;     
-      columnComplete.textContent = toDo.completed;
-      columnName.innerHTML = `<a href="mailto:${userEmailMap.get(toDo.userId)}">${userNameMap.get(toDo.userId)}</a>`;
-      row.append(columnToDo, columnName, columnComplete);
-      tableBody.appendChild(row);       
-    }         
+const tableBody = document.createElement('tbody');
+tableBody.classList.add('tableBody');
+const render = async function () {
+  const toDoList = await fetch(`https://jsonplaceholder.typicode.com/todos`)
+    .then(response => response.json());
+  const usersList = await fetch(`https://jsonplaceholder.typicode.com/users`)
+    .then(response => response.json());
+  const userEmailMap = new Map();
+  const userNameMap = new Map();
+
+  for (let user of usersList) {
+    userEmailMap.set(user.id, user.email);
+    userNameMap.set(user.id, user.name);
   }
-list();
+
+  for (let toDo of toDoList) {
+    const row = document.createElement('tr');    
+    const columnToDo = document.createElement('td');    
+    const columnComplete = document.createElement('td');
+    const columnName = document.createElement('td');  
+    columnToDo.textContent = toDo.title;     
+    columnComplete.textContent = toDo.completed;
+    columnName.innerHTML = `<a href="mailto:${userEmailMap.get(toDo.userId)}">${userNameMap.get(toDo.userId)}</a>`;
+    row.append(columnToDo, columnName, columnComplete);
+    tableBody.appendChild(row);       
+  }         
+}
+render();
 table.appendChild(tableBody);    

acbd87ae9163b1dd4e534360b9e117af57d95c2d
dsrtF0x-git <54433971+dsrtF0x-git@users.noreply.github.com>
2019-10-24 13:07:21 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,23 @@
 'use strict';
 const tableBody = document.getElementsByTagName('tbody')[0];
 
-async function gettingInfo(url)  {
+function gettingInfo(url)  {
   return fetch(url).then(response => response.json());
 }
 
 async function creatingTable() {
   const toDoList = await gettingInfo('https://jsonplaceholder.typicode.com/todos');
   const usersList = await gettingInfo('https://jsonplaceholder.typicode.com/users');
-  console.log(toDoList);
+
   for (const key of toDoList) {
     const tableRow = document.createElement('tr');
     const userEmail = usersList.find(user => user.id === key.userId).email;
     const taskProgress = key.completed ? 'green checkmark icon' : 'window close icon red';
+    
     tableRow.innerHTML = `<td>${key.title}</td>
-                    <td><a href = "mailto:${userEmail}">${userEmail}</a></td>
-                    <td><i class="${taskProgress}"></i>${key.completed}</td>`;
+                          <td><a href = "mailto:${userEmail}">${userEmail}</a></td>
+                          <td><i class="${taskProgress}"></i>${key.completed}</td>`;
+    
     tableBody.append(tableRow);
   }
 }

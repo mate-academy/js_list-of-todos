c85a4b89c91b0a7791a5b736c67369cec15557d4
VadimK <djvadya16@gmail.com>
2019-10-24 15:23:43 +0300

    add some changes

--- src/main.js
+++ src/main.js
@@ -8,20 +8,25 @@ async function getData(url) {
   return response.json(); 
 }
 
-async function test() {
-  let firstPart = await getData('https://jsonplaceholder.typicode.com/todos'); 
-  let secondPart = await getData('https://jsonplaceholder.typicode.com/users'); 
+async function createTable() {
+  const firstPart = await getData('https://jsonplaceholder.typicode.com/todos'); 
+  const secondPart = await getData('https://jsonplaceholder.typicode.com/users'); 
   
   for (let i = 0; i < firstPart.length; i++) {
-    let tr = document.createElement('tr'); 
+    let currentUser = firstPart[i];
+    firstPart.forEach((todos) => {
+      let user = secondPart.find((user) => todos.userId === user.id)
+      currentUser['user'] = user; 
+    })
+    console.log(currentUser); 
+    const tr = document.createElement('tr'); 
     let line = ''; 
-    line += `<td>${firstPart[i].title}</>`; 
-    let currentIdObj = secondPart.find((person) => firstPart[i].userId === person.id);
-    line += `<td>${currentIdObj.email}</td>`; 
-    line += `<td>${firstPart[i].completed}</td>`;
+    line += `<td>${currentUser.title}</>`; 
+    line += `<td>${currentUser.user.email}</td>`; 
+    line += `<td>${currentUser.completed}</td>`;
     tr.innerHTML = line;
     tableBody.append(tr);
   }
 }
 
-test(); 
+createTable(); 

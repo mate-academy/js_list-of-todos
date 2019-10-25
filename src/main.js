fe46433fd9252faf578ee80805f9ba763036337b
Veronika D <donets.veronika@yahoo.com>
2019-10-25 14:59:01 +0300

    solution

---
+++ src/main.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const connection = (url) => {
+  return fetch(url)
+    .then(response => response.json());
+};
+
+const data = async () => {
+  const todos = await connection('https://jsonplaceholder.typicode.com/todos');
+  const users = await connection('https://jsonplaceholder.typicode.com/users');
+
+  const tbody = document.getElementsByClassName('tbody')[0];
+  const keys = Object.keys(todos);
+
+  keys.map(item => {
+    const trow = document.createElement('tr');
+    let value = todos[item];
+    const tbody = document.getElementsByClassName('tbody')[0];
+    const usersEmail = users.find(user => user.id === value.userId).email;
+    const userName= users.find(user => user.id === value.userId).name;
+    let completeness = (value.completed) ? 'completed' : 'failed';
+
+    trow.innerHTML =      `<td>${value.title}</td>
+                          <td><a href = 'mailto:${usersEmail}'>${userName}</a></td>
+                          <td>${completeness}</td>`;
+    tbody.append(trow);
+  });
+};
+
+data();
+
+
+
+
+

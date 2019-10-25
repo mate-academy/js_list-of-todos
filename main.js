20cce03e1163f06b4b279d9a8631271cce479f21
Rudenko Artem <artem.rud@bk.ru>
2019-10-25 14:06:31 +0300

    fix

--- main.js
+++ main.js
@@ -2,18 +2,22 @@ const table = document.getElementById('table');
 const todos = 'https://jsonplaceholder.typicode.com/todos';
 const users = 'https://jsonplaceholder.typicode.com/users';
 
-async function crearedTable() {
-  const todosList = await fetch(todos).then(response => response.json());
-  const todoUsers = await fetch(users).then(response => response.json());
+function fetching(url) {
+  return fetch(url).then(response => response.json());
+}
 
-  for (key of todosList) {
+async function crearedTable() {
+  const todosList = await fetching(todos);
+  const todoUsers = await fetching(users);
+
+  for (todo of todosList) {
     const tr = document.createElement('tr');
-    const [email, name, task] = [todoUsers.find(user => user.id === key.userId).email,
-    todoUsers.find(user => user.id === key.userId).name,
-    key.completed ? 'positive' : 'warning'];
-    tr.innerHTML = `<td>${key.title}</td>
+    const [email, name, task] = [todoUsers.find(user => user.id === todo.userId).email,
+    todoUsers.find(user => user.id === todo.userId).name,
+    todo.completed ? 'positive' : 'warning'];
+    tr.innerHTML = `<td>${todo.title}</td>
                     <td><a href = "mailto:${email}">${name}</a></td>
-                    <td class="${task}">${key.completed ? 'Виконанно' : 'Забито'}</td>`;
+                    <td class="${task}">${todo.completed ? 'Виконанно' : 'Забито'}</td>`;
                     
     table.append(tr);
   }

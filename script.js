08e18a64ec5fdee18f7f31c717d9693f196f1455
Anna Kazantseva <annetastara@gmail.com>
2019-10-26 17:44:09 +0300

    Fixed

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-let urlTodo = 'https://jsonplaceholder.typicode.com/todos';
-let urlUsers = 'https://jsonplaceholder.typicode.com/users';
 let table = document.querySelector('#table');
-let users = fetch(urlUsers)
-  .then(response => response.json());
-let todo = fetch(urlTodo)
-  .then(response => response.json());
-Promise.all([users, todo])
-  .then(([users, todo]) => {
-    todo.forEach(task => {
+
+async function getData(url) {
+  return await fetch(url)
+    .then(response => response.json());
+}
+
+Promise.all([getData('https://jsonplaceholder.typicode.com/users'),
+  getData('https://jsonplaceholder.typicode.com/todos')])
+  .then(([users, todos]) => {
+    todos.forEach(task => {
       let tr = document.createElement('tr');
       tr.insertCell(0).innerHTML = task.title;
       let user = users[task.userId];
       let userNameUrl = document.createElement('a');
+
       userNameUrl.innerHTML = user.name;
       userNameUrl.setAttribute('href', user.email);
       tr.insertCell(1).append(userNameUrl);
       tr.insertCell(2).innerHTML = task.completed;
+
       table.append(tr);
     });
   });

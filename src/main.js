c54ad593d48ec8832060e57644b99587ab2ac87e
Veronika D <donets.veronika@yahoo.com>
2019-10-28 18:22:27 +0200

    fixed js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 'use strict'
 
-const connection = (url) => {
+const getData = (url) => {
   return fetch(url)
     .then(response => response.json());
 };
 
 const data = async () => {
-  const [todos, users] = await Promise.all(
-    [connection(`https://jsonplaceholder.typicode.com/todos`),
-     connection(`https://jsonplaceholder.typicode.com/users`)]);
+  const [todos, users] = await
+    Promise.all(
+     [getData(`https://jsonplaceholder.typicode.com/todos`),
+      getData(`https://jsonplaceholder.typicode.com/users`)
+    ]);
 
   const tbody = document.getElementsByClassName('tbody')[0];
   const keys = Object.keys(todos);
@@ -16,15 +18,18 @@ const data = async () => {
   keys.map(item => {
     const trow = document.createElement('tr');
     const value = todos[item];
-    const userEmail = users.find(user => user.id === value.userId).email;
-    const userName= users.find(user => user.id === value.userId).name;
+    const findUser = users.find(user => user.id === value.userId);
+    const userEmail = findUser.email;
+    const userName= findUser.name;
     const completeness = value.completed ? 'completed' : 'failed';
 
     trow.className = value.completed ? 'positive' : 'negative';
 
-    trow.innerHTML = `<td>${value.title}</td>
-                     <td><a href = 'mailto:${userEmail}'>${userName}</a></td>
-                     <td>${completeness}</td>`;
+    trow.innerHTML = `
+      <td>${value.title}</td>
+      <td><a href = 'mailto:${userEmail}'>${userName}</a></td>
+      <td>${completeness}</td>
+    `;
     tbody.append(trow);
   });
 };

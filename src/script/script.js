2704a8fc1f6e69188dc1d1bfaf338f0d65ed3610
Vitalii Starodub <Vitalii.Starodub@playtech.com>
2019-10-29 14:51:34 +0200

    'added_email_as_reference_to_name'

--- src/script/script.js
+++ src/script/script.js
@@ -4,6 +4,8 @@ const USERS = 'users';
 const TODO_LIST = document.querySelector(".todolist");
 let listOfTodos = [];
 
+const toDoColumns = ['name', 'title', 'completed'];
+
 const getAll = (url) => {
     return fetch(`${BASE_URL}/${url}`)
         .then(response => response.json())
@@ -26,20 +28,35 @@ const generateTableHead = (table, data) => {
     let thead = table.createTHead();
     let row = thead.insertRow();
     for (let key of data) {
-        let th = document.createElement("th");
-        let text = document.createTextNode(key);
-        th.appendChild(text);
-        row.appendChild(th);
+        if (toDoColumns.includes(key)) {
+            let th = document.createElement("th");
+            let text = document.createTextNode(key);
+            th.appendChild(text);
+            row.appendChild(th);
+        }
     }
 };
 const generateTable = (table, data) => {
     for (let element of data) {
         let row = table.insertRow();
         for (let key in element) {
-            let cell = row.insertCell();
-            let text = document.createTextNode(element[key]);
-            cell.appendChild(text);
+            if (toDoColumns.includes(key)) {
+                let cell = row.insertCell();
+                let text = document.createTextNode(element[key]);
+                appendChildToCell(key, text, element[key], cell)
+            }
         }
+    }
+};
+
+const appendChildToCell= (key, text, element, cell) => {
+    if (key === 'name') {
+        let a = document.createElement("a");
+        a.appendChild(text);
+        a.href = element;
+        cell.appendChild(a);
+    } else {
+        cell.appendChild(text);
     }
 };
 
@@ -57,6 +74,7 @@ const appendUserToTodoList = (todolistOld, todoListNew, users) => {
     for (let j = 0; j < users.length; j++) {
         if (users[j].id === todolistOld.userId) {
             todoListNew.name = users[j].name;
+            todoListNew.email = users[j].email;
         }
     }
 };
